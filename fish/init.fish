12e482b71d41824a68d3c0d72ba0b1f77486e962
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2020-10-25 16:59:08 +0900

    :bug: check whether ~/.config/fish/conf.d exists or not when initialization

--- fish/init.fish
+++ fish/init.fish
@@ -3,6 +3,10 @@ cd (dirname (status -f))
 
 set --local dir (pwd)
 
+if not test -e $HOME/.config/fish/conf.d
+   mkdir $HOME/.config/fish/conf.d
+end
+
 # link required files
 for file in (ls -A startup-scripts)
     ln -sf "$dir/startup-scripts/$file" "$HOME/.config/fish/conf.d/$file"
