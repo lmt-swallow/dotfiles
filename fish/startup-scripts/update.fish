3789819931dd8eb313efd728dd960ca93f0c4771
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2020-10-25 15:43:25 +0900

    :sparkles: add auto-update script

---
+++ fish/startup-scripts/update.fish
@@ -0,0 +1,13 @@
+echo '[*] checking for updates ...'
+
+cd ~/.dotfiles
+if git diff --exit-code master origin/master 
+    echo '[*] no update found.'    
+else
+    echo '[*] update found. pulling ...'
+    git pull    
+    echo '[*] done.'
+    
+    echo '[*] you should run the following command to apply this updation.'
+    echo '$ cd ~/.dotfiles; fish init.fish'
+end
