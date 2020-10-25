cf07c23860e187f067256f6717e1008c769885e8
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2020-10-25 15:46:03 +0900

    :bug: add --no-target-directory for ln command to make the init process idempotent

--- emacs/init.fish
+++ emacs/init.fish
@@ -3,5 +3,5 @@ cd (dirname (status -f))
 
 set --local dir (pwd)
 for file in (ls -A | grep -v '*.fish')
-    ln -sf "$dir/$file" "$HOME/$file";
+    ln -sf --no-target-directory "$dir/$file" "$HOME/$file";
 end
