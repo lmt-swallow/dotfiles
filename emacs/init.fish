7beff757a97e5fa0e67f8c9565501b8d4584219e
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2020-10-28 02:26:34 +0900

    :recycle: exclude .emacs.d from management

--- emacs/init.fish
+++ emacs/init.fish
@@ -3,10 +3,6 @@ cd (dirname (status -f))
 
 set --local dir (pwd)
 
-if test -d "$HOME/.emacs.d" 
-   /bin/rm -rf "$HOME/.emacs.d"
-end
-
 for file in (ls -A | grep -v '*.fish')
     ln -sf --no-target-directory "$dir/$file" "$HOME/$file";
 end
